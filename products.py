c20970c7b6280ce04275396f8390973db9708248
Lucas Lin <lucaslin0123@icloud.com>
2022-07-20 15:52:56 +0800

    Read file

--- products.py
+++ products.py
@@ -1,4 +1,11 @@
 products = []
+with open('products.txt','r') as f:
+	for line in f:
+		name, price = line.strip().split(',')
+		products.append([name, price])
+
+print(products)
+		
 while True:
 	name = input('Enter name of the products:')
 	if name == 'q':
@@ -11,3 +18,8 @@ while True:
 
 for p in products:
 	print(p[0],'cost',p[1])
+
+with open('products.txt','w') as f:
+	f.write('products,price\n')
+	for p in products:
+		f.write(p[0]+','+p[1]+'\n')
