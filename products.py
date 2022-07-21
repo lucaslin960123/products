90285c78e6e0e59073c401a824ca285266b95e59
Lucas Lin <lucaslin0123@icloud.com>
2022-07-21 16:22:25 +0800

    New version

--- products.py
+++ products.py
@@ -1,42 +1,49 @@
 import os
 
-products = []
-
-
-
-if os.path.isfile('products.txt'):
-	print('Found the file!')
-	with open('products.txt','r') as f:
+def read_file(filename):
+	products = []
+	with open(filename,'r') as f:
 		for line in f:
 			if 'products,price' in line:
 				continue
 		name, price = line.strip().split(',')
 		products.append([name, price])
+	return products
+			
+def user_input(products):		
+	while True:
+		name = input('Enter name of the products:')
+		if name == 'q':
+			break
+		else:
+			price = input('Enter the price if the products:')
+			p = []
+			p = [name, price]
+			products.append(p)
 	print(products)
+	return products
 
-else:
-	print('Did not find the file')
-
-
-		
-while True:
-	name = input('Enter name of the products:')
-	if name == 'q':
-		break
-	else:
-		price = input('Enter the price if the products:')
-		p = []
-		p = [name, price]
-		products.append(p)
-
-
-
-for p in products:
-	print(p[0],'cost',p[1])
-
-
-
-with open('products.txt','w') as f:
-	f.write('products,price\n')
+def print_products(products):
 	for p in products:
-		f.write(p[0]+','+p[1]+'\n')
+		print(p[0],'cost',p[1])
+
+def write_file(filename,products):
+	with open(filename,'w') as f:
+		f.write('products,price\n')
+		for p in products:
+			f.write(p[0]+','+p[1]+'\n')
+
+def main():
+	filename = 'products.txt'
+	if os.path.isfile(filename):
+			print('Found the file')
+			products = read_file(filename)
+
+	else:
+		print('Did not find the file')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+main()
